f291b6249d28ced8608a0b82db18d91eac51310d
nasserrgithub <34746890+nasserrgithub@users.noreply.github.com>
2020-09-18 16:09:58 +0800

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,11 @@ const db = knex ({
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 
 //controllers
